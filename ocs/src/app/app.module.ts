17f50a66cd7f4a94c24e39ef4d60f4dcdb0c71fb
aniket <aniket@gmail.com>
2021-11-01 11:56:45 -0930

    all commit

---
+++ ocs/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { AboutComponent } from './main/about/about.component';
+import { ServicesComponent } from './main/services/services.component';
+import { CareerComponent } from './main/career/career.component';
+import { ProductComponent } from './main/product/product.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    AboutComponent,
+    ServicesComponent,
+    CareerComponent,
+    ProductComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
